ec795542e5881cd6ff2b11a64b8b1709ce0b1e0b
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-15 10:51:01 +0100

    Homepage no styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./components/footer/footer.component";
+import { MenuComponent } from "./components/menu/menu.component";
+import { NgClass } from '@angular/common';
+import { ScrollToTopService } from '../services/scroll-to-top-service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, MenuComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bench-frontend';
+  constructor(private scrollToTopService: ScrollToTopService) {
+    // The ScrollToTopService is automatically activated through the constructor
+  }
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    this.isScrolled = scrollTop > 0;
+  }
 }
+
