e7a2b44dcc9dffc92cd1388b6a434baaf9da7b4a
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-01-15 11:07:53 +0100

    basic route

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  hideMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
