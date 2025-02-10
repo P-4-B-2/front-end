9384738e37e6dd4a7e33beeaafefc58a090150c8
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-02-10 10:25:57 +0100

    Basic map

--- src/app/pages/bench-location/bench-location.component.ts
+++ src/app/pages/bench-location/bench-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-bench-location',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
   templateUrl: './bench-location.component.html',
   styleUrl: './bench-location.component.css'
 })
-export class BenchLocationComponent {
+export class BenchLocationComponent implements OnInit {
+  private map!: L.Map;
+  private marker!: L.Marker;
 
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    // 51.16405955699206 4.988548279070529
+    this.map = L.map('map').setView([51.16405955699206, 4.988548279070529], 13); // Geel
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    this.map.on('click', (event: L.LeafletMouseEvent) => {
+      const { lat, lng } = event.latlng;
+      this.setMarker(lat, lng);
+      console.log('Selected Coordinates:', lat, lng);
+    });
+  }
+
+  private setMarker(lat: number, lng: number): void {
+    if (this.marker) {
+      this.map.removeLayer(this.marker);
+    }
+    this.marker = L.marker([lat, lng]).addTo(this.map);
+  }
 }
