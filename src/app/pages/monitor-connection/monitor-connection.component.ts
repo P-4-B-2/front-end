c33dd27a6b8f747a7298d1172e478c387b454b33
Sebastiaan Daniels <r0929429@student.thomasmore.be>
2025-02-13 10:19:39 +0100

    Basic studs

---
+++ src/app/pages/monitor-connection/monitor-connection.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-monitor-connection',
+  standalone: true,
+  imports: [],
+  templateUrl: './monitor-connection.component.html',
+  styleUrl: './monitor-connection.component.css'
+})
+export class MonitorConnectionComponent {
+
+}
